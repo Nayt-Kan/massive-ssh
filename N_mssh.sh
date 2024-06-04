4352c1c1020a4cab0354cd4a9f1a1e3a52b56c1a
Nayt-Kan <nikita.ivannikov.mikh@gmail.com>
2024-06-04 07:44:03 -0400

    Recognition of comments and empty lines

--- N_mssh.sh
+++ N_mssh.sh
@@ -25,7 +25,7 @@ hosts=hosts.txt
 d_port=2222
 d_user=user
 timeout=10
-max_threads=5
+max_threads=20
 
 #ssh_id=/home/user/.ssh/id_rsa
 #ssh_id=~/.ssh/id_rsa
@@ -89,7 +89,7 @@ fi
 
 #==||reassigning parameters||==#
 
-lines=`wc -l $hosts | cut -d" " -f1`
+lines=`wc -l $hosts | cut -d' ' -f1`
 [ $lines -lt $max_threads ] && max_threads=$lines
 
 #==||preparing hosts||==#
@@ -100,14 +100,18 @@ do
 ((j++))
 (
 line="${line//$'\r'/}" # for win files; changes End Of Line(win) to Line Feed (unix)
+line=`echo $line | cut -d# -f1`
 
 host=`echo $line | grep -E "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | cut -d@ -f2 | cut -d: -f1`
 user=`echo $line | grep @ | cut -d@ -f1`
 port=`echo $line | grep : | cut -d: -f2`
 
-[ -n "$host" ] || sc=201
 [ -n "$user" ] || user=$d_user
 [ -n "$port" ] || port=$d_port
+
+[ -n "$host" ] || sc=201
+[ -n "$line" ] || sc=202
+
 
 #==||preparing request||==#
 
@@ -117,13 +121,13 @@ then
   if [ -n "$mcp" ]
   then
     scp -o ConnectTimeout=$timeout -P $port -i $ssh_id $from $user@$host':'$to &>>results.log
-  [ $? != 0 ] && sc=203 || echo "scp for line $j ready"
+  [ $? != 0 ] && sc=301 || echo "scp for line $j ready"
   fi
   #==||ssh||==#
   if [ -n "$msh" ]
   then
     ssh -o ConnectTimeout=$timeout -p $port -i $ssh_id $user@$host $shcmd &>>results.log
-    [ $? != 0 ] && sc=202 || echo "ssh for line $j redy"
+    [ $? != 0 ] && sc=302 || echo "ssh for line $j ready"
   fi
 fi
 
@@ -134,10 +138,11 @@ then
   case "$sc" in
     201) echo $line >> wrong_host.log
          echo "line $j is a wrong" ;;
-    202) echo $line >> timeout.log
-         echo "ssh for line $j N/A" ;;
-    203) echo $host >> timeout.log
+    202) echo test ;;
+    301) echo $host >> timeout.log
          echo "scp for line $j N/A" ;;
+    302) echo $line >> timeout.log
+         echo "ssh for line $j N/A" ;;
       *) echo "sc = $sc" ;;
   esac
 fi
